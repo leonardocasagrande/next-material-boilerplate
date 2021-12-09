8d1406f692735cf8efed01d98e0b4fe04cca44a8
Leonardo Casagrande <leonardo.casagrande@hitechnologies.com.br>
2021-12-09 12:23:47 -0300

    Added dependencies and functions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material';
 import { AppContextProvider } from 'hooks/AppContext';
 import type { AppProps } from 'next/app';
 import theme from 'theme';
-import '../config/i18n';
+import 'config/i18n';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
